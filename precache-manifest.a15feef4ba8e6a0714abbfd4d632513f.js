448fd9c4ce686249aeb51d2bb96b27ff79905a8f
AanchalCh <aanchal.chaurasia99@gmail.com>
2020-07-23 19:46:20 +0530

    Updates

---
+++ precache-manifest.a15feef4ba8e6a0714abbfd4d632513f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc649d2ca49807442e59e52adb245b6c",
+    "url": "/ConFusion/index.html"
+  },
+  {
+    "revision": "bd800e24d908f71b3c75",
+    "url": "/ConFusion/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b13c5624948f83478922",
+    "url": "/ConFusion/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bd800e24d908f71b3c75",
+    "url": "/ConFusion/static/js/2.024653fc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ConFusion/static/js/2.024653fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13c5624948f83478922",
+    "url": "/ConFusion/static/js/main.ff7efaf7.chunk.js"
+  },
+  {
+    "revision": "b3807705ec60840bdb77",
+    "url": "/ConFusion/static/js/runtime-main.c05b6654.js"
+  }
+]);
